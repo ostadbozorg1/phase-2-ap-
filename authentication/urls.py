cfacab5d3a8dc3955ddcad9536113f94db94f512
Maf313 <156673680+Maf313@users.noreply.github.com>
2024-02-02 11:35:10 +0330

    url and views

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.check_login_or_main, name='send_otp'),
     path('login/', views.login_page, name='send_otp'),
     path('do_login/', views.do_login, name='send_otp'),
+    path('do_signup/', views.do_signup, name='send_otp'),
+    path('do_add_clinic/', views.do_add_clinic, name='send_otp'),
+
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -25,3 +25,69 @@ def do_login(request):
     else:
         print("umm")
         return redirect('/login?msg=Method not allowed')
+
+
+def do_signup(request):
+    print("man zendam")
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        email = request.POST['email']
+        name = request.POST['name']
+        role = request.POST['role']
+        clinic_id = request.POST['clinic_id']
+        if(role != "monshi" and role != "patient"):
+            return redirect('/login?msg=what is that role you entered bro')
+        try:
+            user = UserModel.objects.get(username=username)
+            return redirect('/login?msg=duplicate username')
+        except:
+            pass
+        try:
+            user = UserModel.objects.get(email=email)
+            return redirect('/login?msg=duplicate email')
+        except:
+            pass
+        if(role == "monshi"):
+            if(len(clinic_id) < 1):
+                return redirect('/login?msg=wrong clinic id')
+            try:
+                user = UserModel.objects.get(clinic_id=clinic_id)
+                return redirect('/login?msg=wrong clinic id')
+            except:
+                pass
+        if(len(clinic_id) < 1):
+            clinic_id = "not_monshi"
+        user = UserModel(username=username, password=password,clinic_id=clinic_id,email=email,name=name,role=role)
+        user.save()
+        request.session['username'] = username
+        return redirect('/panel/?msg=Welcome')
+    else:
+        print("ummmm")
+        return redirect('/login?msg=Method not allowed')
+
+def do_add_clinic(request):
+    print("man zendam")
+    if request.method == 'POST':
+        clinic_id = request.POST['clinic_id']
+        name = request.POST['name']
+        email = request.POST['email']
+        address = request.POST['address']
+        phone_number = request.POST['phone_number']
+        services = request.POST['services']
+        try:
+            user = ClinicModel.objects.get(email=email)
+            return redirect('/login?msg=dup email')
+        except:
+            pass
+        try:
+            user = ClinicModel.objects.get(clinic_id=clinic_id)
+            return redirect('/login?msg=dup clinic id')
+        except:
+            pass
+        clinic = ClinicModel(clinic_id=clinic_id, email=email,name=name,address=address,phone_number=phone_number, services=services)
+        clinic.save()
+        return redirect('/panel/')
+    else:
+        print("ummmm")
+        return redirect('/login?msg=method not allowed')
