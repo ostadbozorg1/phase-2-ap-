1ce383f8b1ce5fa0481cbda2330ee569d72f9840
Ali Haji <ali.hajialiakbar2@gmail.com>
2024-02-02 16:10:24 +0330

    nobat view 2

--- nobat/urls.py
+++ nobat/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.panel, name='send_otp'),
     path('new_appointment/', views.new_appointment, name='send_otp'),
     path('make_new_appointment/', views.make_new_appointment, name='send_otp'),
+    path('my_appointments/', views.my_appointments, name='send_otp'),
+    path('cancel_appointment/', views.cancel_appointment, name='send_otp'),
+    path('list_clinics/', views.list_clinics, name='send_otp'),
+    path('logout/', views.make_logout, name='send_otp'),
 ]
--- nobat/views.py
+++ nobat/views.py
@@ -90,3 +90,84 @@ def make_new_appointment(request):
         appointment.save()
         return redirect('../my_appointments/?msg=Success')
     
+
+def my_appointments(request):
+    msg = request.GET.get('msg', "nope")
+    msg2 = msg.replace(" ","")
+    if(msg2.isalpha() is not True):
+        msg="nope"
+    if(msg == "nope"):
+        html = ""
+    else:
+        html = "<p>Message: "+str(msg)+"</p>"
+    if 'username' not in request.session:
+        return redirect('../../login/?msg=Please login')
+    user = UserModel.objects.get(username=request.session['username'])
+    if(user.role != "patient"):
+        return redirect('../panel/?msg=Access Denied')
+    
+    Appointments = Appointment.objects.filter(user_id=request.session['username'])
+    html += '<table><tr><th>Date</th><th>Clinic ID</th><th>Status</th><th>Actions</th></tr>'
+
+    # Add a row for each appointment
+    for appointment in Appointments:
+        html += f'<tr><td>{appointment.date}</td><td>{appointment.clinic_id}</td><td>{appointment.status}</td><td><a href="/panel/cancel_appointment/?id={appointment.id}">Cancel</a></td></tr>'
+
+    # End of the HTML table
+    html += '</table><br><br><a href="../../panel">Return to panel</a>'
+
+    return HttpResponse(html)
+    
+
+def cancel_appointment(request):
+    if 'username' not in request.session:
+        return redirect('../my_appointments/?msg=Please login')
+    appoint_id = request.GET.get('id', "nope")
+    if appoint_id == "nope" :
+        return redirect('../login/?msg=Haaaaa')
+    try:
+        appoint = Appointment.objects.get(id=appoint_id)
+    except:
+        return redirect('../my_appointments/?msg=Wrong appointment id')
+    user = UserModel.objects.get(username=request.session['username'])
+    if(appoint.user_id != user.username):
+        if(user.role != "monshi" or user.clinic_id != appoint.clinic_id):
+            return redirect('../my_appointments/')
+    appoint.status = "canceled"
+    appoint.save()
+    if(user.role == "patient"):
+        return redirect('../my_appointments/')
+    return redirect('../pending_appointments/')
+
+def approve_appointment(request):
+    if 'username' not in request.session:
+        return redirect('../my_appointments/?msg=Please login')
+    appoint_id = request.GET.get('id', "nope")
+    if appoint_id == "nope" :
+        return redirect('../login/?msg=Haaaaaaaaaa')
+    try:
+        appoint = Appointment.objects.get(id=appoint_id)
+    except:
+        return redirect('../my_appointments/?msg=Wrong id')
+    user = UserModel.objects.get(username=request.session['username'])
+    if(user.role != "monshi" or user.clinic_id != appoint.clinic_id):
+        return redirect('../panel/?msg=Access Denied')
+    appoint.status = "approved"
+    appoint.save()
+    return redirect('../pending_appointments/?msg=Success')
+def list_clinics(request):
+    Clinics = ClinicModel.objects.all()
+    html = '<h1>List of Clinics</h1><br><table><tr><th>Name</th><th>Clinic ID</th><th>Email</th><th>Address</th><th>Phone Number</th><th>Services</th></tr>'
+
+    # Add a row for each appointment
+    for clinic in Clinics:
+        html += f'<tr><td>{clinic.name}</td><td>{clinic.clinic_id}</td><td>{clinic.email}</td><td>{clinic.address}</td><td>{clinic.phone_number}</td><td>{clinic.services}</td></tr>'
+
+    # End of the HTML table
+    html += '</table><br><br><a href="../../panel">Return to panel</a>'
+
+    return HttpResponse(html)
+
+def make_logout(request):
+    logout(request)
+    return redirect('../?msg=Bye')
